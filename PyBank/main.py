3ff6a632c337e327d7ecfb1a64506dbdff2e02b8
niccc <nfernandocolon@gmail.com>
2020-03-29 21:56:28 -0400

    added more comments and writing to file

--- PyBank/main.py
+++ PyBank/main.py
@@ -20,13 +20,16 @@ with open(budget_data_csv, 'r') as csvfile:
     
     #skip header
     header = next(csvreader)
-
     #loop through data
     for row in csvreader:
+        #update total 
         total_profit += float(row[1])
+        #calculate change in profit
         profit_change = float(row[1]) - previous_profit 
+        #store change in progit
         profit_change_list.append(profit_change)
 
+        #determine the greatest profit/loss
         if float(row[1]) > greatest_profit:
             greatest_profit = float(row[1])
             greatest_profit_month = row[0] 
@@ -34,9 +37,12 @@ with open(budget_data_csv, 'r') as csvfile:
             greatest_loss = float(row[1])
             greatest_loss_month = row[0]
         
+        #save current profit value for use in next iteration as previous profit
         previous_profit = float(row[1])
+        
+        #countin months 
         month_counter += 1
-    
+#getting rid of bogus first value in the profit change list    
 del profit_change_list[0]
 avg_change = sum(profit_change_list)/len(profit_change_list)
 
@@ -48,7 +54,16 @@ print(f"Average Change: ${avg_change}")
 print(f"Greatest Increase in Profits: {greatest_profit_month} (${greatest_profit})")
 print(f"Greatest Decrease in Profits: {greatest_loss_month} (${greatest_loss})")
 
-
+#write to file
+output_path = os.path.join('.', 'output_file.txt')
+with open(output_path, 'w') as output_file:
+    print("Financial Analysis", file = output_file)
+    print("----------------------------", file = output_file)
+    print(f"Total Months: {month_counter}", file = output_file)
+    print(f"Total: ${total_profit}", file = output_file)
+    print(f"Average Change: ${avg_change}", file = output_file)
+    print(f"Greatest Increase in Profits: {greatest_profit_month} (${greatest_profit})", file = output_file)
+    print(f"Greatest Decrease in Profits: {greatest_loss_month} (${greatest_loss})", file = output_file)        
 
 
 
